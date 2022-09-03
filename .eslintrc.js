1ed31195bedb9d5ebc87e19edfb00de1ba9025b3
mechcodeman <1067327983@qq.com>
2022-09-03 17:03:09 +0800

    add Home Login and debugged

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,13 @@ module.exports = {
     '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser'
   },
   rules: {
+    'space-before-function-paren': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'space-before-function-paren': 0
+    'vue/multi-word-component-names': 'off'
   }
 }
