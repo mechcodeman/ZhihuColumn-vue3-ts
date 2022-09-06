5deca3dd0bcfa0fb73f38e9d8b2b10f5238afcd5
mechcodeman <1067327983@qq.com>
2022-09-06 19:50:32 +0800

    引入路由守卫并演示使用

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import ColumnDetail from './views/ColumnDetail.vue'
 import CreatePost from './views/CreatePost.vue'
+import store from './store'
 const routerHistory = createWebHistory()
 const router = createRouter({
   history: routerHistory,
@@ -29,5 +30,11 @@ const router = createRouter({
     }
   ]
 })
-
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'login' && !store.state.user.isLogin) { // 如果用户未登录且去往非登录界面，则重定向到登录界面，否则继续
+    next({ name: 'login' })
+  } else {
+    next()
+  }
+})
 export default router
