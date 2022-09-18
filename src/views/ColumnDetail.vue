557323384f3a05cdbd2a0ccd7ae204e0726ed408
mechcodeman <1067327983@qq.com>
2022-09-18 22:08:17 +0800

    ColumnDetail bug fix

--- src/views/ColumnDetail.vue
+++ src/views/ColumnDetail.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, onMounted } from 'vue'
+import { defineComponent, computed, onMounted, watch, ref } from 'vue'
 import { useRoute } from 'vue-router' // 获取路由信息
 import { useStore } from 'vuex'
 import { GlobalDataProps, ColumnProps } from '../store'
@@ -29,19 +29,31 @@ export default defineComponent({
   setup() {
     const store = useStore<GlobalDataProps>()
     const route = useRoute()
-    const currentId = route.params.id // currentId已经是字符串类型数据，目的在于动态存储当前用户的id信息
+    // 为了让 computed 响应对应的变化，添加响应式对象
+    const currentId = ref(route.params.id)
     onMounted(() => {
-      store.dispatch('fetchColumn', currentId)
-      store.dispatch('fetchPosts', currentId)
+      store.dispatch('fetchColumn', currentId.value)
+      store.dispatch('fetchPosts', currentId.value)
+    })
+    // 检测变化
+    watch(() => route.params, (toParams) => {
+      // 确保要变化的路径是进入到用户的专栏
+      if ((toParams && toParams.id) === store.state.user.column) {
+        // 重新发送请求，在 store 中有对应的缓存设置
+        store.dispatch('fetchColumn', toParams.id)
+        store.dispatch('fetchPosts', toParams.id)
+        // 重新赋值，这样 computed 会变化
+        currentId.value = toParams.id
+      }
     })
     const column = computed(() => {
-      const selectColumn = store.getters.getColumnById(currentId) as ColumnProps | undefined
+      const selectColumn = store.getters.getColumnById(currentId.value) as ColumnProps | undefined
       if (selectColumn) {
         addColumnAvatar(selectColumn, 100, 100)
       }
       return selectColumn
     })
-    const list = computed(() => store.getters.getPostsByCid(currentId))
+    const list = computed(() => store.getters.getPostsByCid(currentId.value))
     return {
       column,
       list
